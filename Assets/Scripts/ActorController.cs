154620a3faf66c36b6d27be549b48c45cb3e2c4a
Robert Hoff <robbyhoff95@gmail.com>
2026-05-17 21:32:33 -0400

    Separates input and controller

---
+++ Assets/Scripts/ActorController.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class ActorController : MonoBehaviour
+{
+    #region Public
+    
+    [Header("Visuals")]
+    public GameObject vfx;
+    
+    [Header("Movement")]
+    public Vector2 initialVelocity;
+    [Range(0f, 1f)]public float linearVelocitySmoothTime;
+    [Range(0f, 100f)] public float thrustAcceleration;
+    [Range(0f, 100f)] public float gravityAcceleration;
+    [Range(0f, 100f)] public float buoyancyAcceleration;
+    [Range(0f, 100f)] public float maxSpeed;
+    
+    [Header("Rotation")]
+    [Range(0f, 500f)] public float rotationSpeedNoThrust;
+    [Range(0f, 500f)] public float rotationSpeedThrust;
+    
+    #endregion
+    
+    #region Private
+
+    private Rigidbody2D _rb;
+    private Vector2 _linearVelocity;
+    private float _direction;
+
+    // Inputs
+    private bool _thrustActive;
+    private float _rotationInput;
+
+    #endregion
+    
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+    
+    private void Start()
+    {
+        _rb.linearVelocity = initialVelocity;
+    }
+    
+    private void Update()
+    {
+
+        vfx.transform.rotation = Quaternion.Euler(-_direction, 90, _direction);
+    }
+    
+    private void FixedUpdate()
+    {
+        var rotationSpeed = _thrustActive ? rotationSpeedThrust : rotationSpeedNoThrust;
+        
+        _direction += Time.deltaTime * _rotationInput * rotationSpeed;
+        _direction %= 360;
+        
+        var radians = _direction * Mathf.Deg2Rad;
+        var direction = new Vector2(Mathf.Cos(radians), Mathf.Sin(radians));
+
+        // Thrust based on input
+        if (_thrustActive)
+        {
+            // _rb.linearVelocity += direction.normalized * (Time.fixedDeltaTime * (_thrustInput * thrustAcceleration));
+            _rb.linearVelocity = Vector2.SmoothDamp(
+                _rb.linearVelocity,
+                direction.normalized * thrustAcceleration,
+                ref _linearVelocity,
+                linearVelocitySmoothTime
+            );
+        }
+        // Downward gravity if no thrust and above horizon
+        else if (transform.position.y > 0f)
+        {
+            _rb.linearVelocity += Time.fixedDeltaTime * new Vector2(0f, -gravityAcceleration);
+        }
+
+        if (transform.position.y < 0f)
+        {
+            if (_rb.linearVelocity.y > 0f)
+            {
+                _rb.linearVelocity += Time.fixedDeltaTime * new Vector2(0f, buoyancyAcceleration);
+            }
+            else
+            {
+                _rb.linearVelocity += Time.fixedDeltaTime * new Vector2(0f, buoyancyAcceleration);
+            }
+        }
+
+        if (_rb.linearVelocity.magnitude > maxSpeed)
+        {
+            _rb.linearVelocity = _rb.linearVelocity.normalized * maxSpeed;
+        }
+    }
+    
+    private void OnDrawGizmos()
+    {
+        if (_rb != null)
+        {
+            Gizmos.DrawRay(transform.position, _rb.linearVelocity);
+        }
+    }
+
+    public void SetThrustActive(bool thrustActive)
+    {
+        _thrustActive = thrustActive;
+    }
+    
+    public void SetRotationInput(float rotationInput)
+    {
+        _rotationInput = rotationInput;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,51 +1,21 @@
-using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Rigidbody2D), typeof(ActorController))]
 public class PlayerController : MonoBehaviour, PlayerActions.IGameplayActions
 {
     private PlayerActions _playerActions;
     private PlayerActions.GameplayActions _gameplayActions;
+    private ActorController _actorController;
 
-    private Rigidbody2D _rb;
-
-    private float _thrustInput;
-    private float _rotateInput;
-
-    private float _direction = 0f;
-
-    public GameObject vfx;
     public ParticleSystem thrustPfx;
-
-    public Vector2 initialVelocity;
-   
-    private Vector2 _linearVelocity;
-    
-    [Header("Movement")]
-    [Range(0f, 1f)]public float linearVelocitySmoothTime;
-    [Range(0f, 100f)] public float thrustAcceleration;
-    [Range(0f, 100f)] public float gravityAcceleration;
-    [Range(0f, 100f)] public float buoyancyAccelerationGoingDown;
-    [Range(0f, 100f)] public float buoyancyAccelerationGoingUp;
-    [Range(0f, 100f)] public float maxSpeed;
-    
-    [Header("Rotation")]
-    [Range(0f, 500f)] public float rotationSpeedNoThrust;
-    [Range(0f, 500f)] public float rotationSpeedThrust;
 
     private void Awake()
     {
         _playerActions = new PlayerActions();
         _gameplayActions = _playerActions.Gameplay;
         _gameplayActions.AddCallbacks(this);
-
-        _rb = GetComponent<Rigidbody2D>();
-    }
-
-    private void Start()
-    {
-        _rb.linearVelocity = initialVelocity;
+        _actorController = GetComponent<ActorController>();
     }
 
     private void OnDestroy()
@@ -71,12 +41,12 @@ public class PlayerController : MonoBehaviour, PlayerActions.IGameplayActions
 
         if (value)
         {
-            _thrustInput = 1f;
+            _actorController.SetThrustActive(true);
             thrustPfx.Play();
         }
         else
         {
-            _thrustInput = 0f;
+            _actorController.SetThrustActive(false);
             thrustPfx.Stop();
         }
     }
@@ -84,68 +54,8 @@ public class PlayerController : MonoBehaviour, PlayerActions.IGameplayActions
     public void OnTurn(InputAction.CallbackContext context)
     {
         var value = context.ReadValue<float>();
-
-        _rotateInput = value;
+        _actorController.SetRotationInput(value);
     }
 
     #endregion
-
-    private void Update()
-    {
-
-        vfx.transform.rotation = Quaternion.Euler(-_direction, 90, _direction);
-    }
-    
-    private void FixedUpdate()
-    {
-        var rotationSpeed = _thrustInput > 0f ? rotationSpeedThrust : rotationSpeedNoThrust;
-        
-        _direction += Time.deltaTime * _rotateInput * rotationSpeed;
-        _direction %= 360;
-        
-        var radians = _direction * Mathf.Deg2Rad;
-        var direction = new Vector2(Mathf.Cos(radians), Mathf.Sin(radians));
-
-        // Thrust based on input
-        if (_thrustInput > 0f)
-        {
-            // _rb.linearVelocity += direction.normalized * (Time.fixedDeltaTime * (_thrustInput * thrustAcceleration));
-            _rb.linearVelocity = Vector2.SmoothDamp(
-                _rb.linearVelocity,
-                direction.normalized * (_thrustInput * thrustAcceleration),
-                ref _linearVelocity,
-                linearVelocitySmoothTime
-            );
-        }
-        // Downward gravity if no thrust and above horizon
-        else if (transform.position.y > 0f)
-        {
-            _rb.linearVelocity += Time.fixedDeltaTime * new Vector2(0f, -gravityAcceleration);
-        }
-
-        if (transform.position.y < 0f)
-        {
-            if (_rb.linearVelocity.y > 0f)
-            {
-                _rb.linearVelocity += Time.fixedDeltaTime * new Vector2(0f, buoyancyAccelerationGoingUp);
-            }
-            else
-            {
-                _rb.linearVelocity += Time.fixedDeltaTime * new Vector2(0f, buoyancyAccelerationGoingDown);
-            }
-        }
-
-        if (_rb.linearVelocity.magnitude > maxSpeed)
-        {
-            _rb.linearVelocity = _rb.linearVelocity.normalized * maxSpeed;
-        }
-    }
-
-    private void OnDrawGizmos()
-    {
-        if (_rb != null)
-        {
-            Gizmos.DrawRay(transform.position, _rb.linearVelocity);
-        }
-    }
 }
